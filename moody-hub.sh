fcdb512ef43a9fbc88b7f9d306fd9ef5be05532c
mar <gim.marcello@gmail.com>
2021-01-29 20:58:04 +0100

    fixed bugs in moody gw run script (broker, closing processes)

--- moody-hub.sh
+++ moody-hub.sh
@@ -9,23 +9,37 @@
 #     sudo apt install golang-go nodejs npm mosquitto
 #
 
-
 usage() {
   printf "Starts the moody hub/gw; mosquitto, node+npm and go are required to build and run the application\n"
   printf "\t[-h] Display this message\n"
   printf "\t[-s] Starts the hub in background\n"
 }
 
-check_running() {
-  for pfile in bin/tmp/*.pid; do
-    [ -f "$pfile" ] && echo "An instance of the app is already running, close with ./moody-hub.sh -c"; exit 1;
-  done
+bin_config() {
+  custom_mosq='autosave_interval 1800
+persistence true
+retained_persistence true
+persistence_file m2.db
+persistence_location '$1'
+connection_messages true
+log_timestamp true
+
+
+listener 8883
+cafile '$1'/ca.crt
+certfile '$1'/server.crt
+keyfile '$1'/server.key'
+  echo "$custom_mosq" > "$1/mosquitto.conf"
 }
 
+close_all() {
+  kill "$(cat bin/tmp/to-close)"
+}
 
-close() {
-  for pid_file in bin/tmp/*; do
-    kill -INT "$(cat "$pid_file")" && rm "$pid_file"
+check_running() {
+  shopt -s nullglob
+  for pfile in bin/tmp/*; do
+    [ -f "$pfile" ] && echo "An instance of the app is already running, close with ./moody-hub.sh -c"; exit 1;
   done
 }
 
@@ -34,46 +48,43 @@ while getopts ":hsc" opt; do
   case $opt in
     h ) usage; exit 0;;
     s ) background=1;;
-    c ) close; exit 0;;
-    : ) echo "Missing argument for option -$OPTARG"; usage; exit 1;;
+    c ) close_all; exit 0;;
     \?) echo "Unknown option -$OPTARG"; usage; exit 1;;
   esac
 done
 
 # check for build/run dependencies
+mkdir -p bin/log
+mkdir -p bin/tmp
 
 check_running
 
 mosq_check=$(command -v mosquitto)
-[ -z "$mosq_check" ] && echo "Missing dependency: mosquitto"; exit 1;
+[ -z "$mosq_check" ] && echo "Missing dependency: mosquitto" && exit 1;
 
 go_check=$(command -v go)
-[ -z "$go_check" ] && echo "Missing dependency: golang-go"; exit 1;
+[ -z "$go_check" ] && echo "Missing dependency: golang-go" && exit 1;
 
 npm_check=$(command -v npm)
-[ -z "$npm_check" ] && echo "Missing dependency: npm"; exit 1;
+[ -z "$npm_check" ] && echo "Missing dependency: npm" && exit 1;
 
 
 # build the bin/ folder is not present
 
-[ -d bin/ ] || echo "Building the moody front architecture"; make build-front
+[ -d bin/ ] || (echo "Building the moody front architecture" && make build-front)
 [ -z "$(command -v serve)" ] && npm install -g serve
+[ -d bin/broker ] || (mkdir -p bin/broker && cp broker/* bin/broker && bin_config "$PWD/bin/broker")
 
-# run and save pid to file for a later close
+# run everything and save logs
 
-mkdir -p bin/log
-mkdir -p bin/tmp
-
-mosquitto -c ./broker/mosquitto.conf -v > bin/log/mosquitto.log 2> bin/log/mosquitto.log &
-echo $! > bin/tmp/mosquitto.pid
-
-serve -l 3000 -s bin/build &
-echo $! > bin/tmp/webapp.pid
+trap 'kill $(jobs -p)' SIGINT
+mosquitto -c bin/broker/mosquitto.conf -v > bin/log/mosquitto.log 2> bin/log/mosquitto.log &
+serve -l 3000 -s bin/build > /dev/null 2> /dev/null &
 
 if [ -z $background ]; then
-  GODEBUG="x509ignoreCN=0" ./bin/gateway 2>&1 | tee -a bin/log/gateway.log
-  close
+  (cd bin || exit 1; GODEBUG="x509ignoreCN=0" ./gateway 2>&1 | tee -a log/gateway.log)
+  echo "Bye!"
 else
-  GODEBUG="x509ignoreCN=0" ./bin/gateway 2>&1 | tee -a bin/log/gateway.log &
-  echo $! > bin/tmp/gateway.pid
+  (cd bin || exit 1; GODEBUG="x509ignoreCN=0" ./gateway 2>&1 | tee -a log/gateway.log &)
+  jobs -p > bin/tmp/to-close
 fi
